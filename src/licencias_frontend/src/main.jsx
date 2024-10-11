832339835499063aa06be9319417d1e6859e1b1f
ronaldolara1 <utm23090401@utma.edu.mx.com>
2024-10-11 13:27:50 -0600

    RUTAS

--- src/licencias_frontend/src/main.jsx
+++ src/licencias_frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,RouterProvider,
+} from "react-router-dom";
 import App from './App';
 import './index.scss';
+import Login from './Login';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+  },
+  {
+    path:"/Login",
+    element: <Login/>,
+  },
+  {
+    path:"/a",
+    element: {},
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
