0b91f444a8b17a534b8ccd2e90233580b8095ce0
MarioAlarconA <utm23090584@utma.edu.mx>
2024-10-11 10:56:32 -0600

    Cambios en login

--- src/licencias_frontend/src/App.jsx
+++ src/licencias_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { licencias_backend } from 'declarations/licencias_backend';
-import { Container, Nav, Navbar, NavDropdown,Button } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown, Button, Card, Form, FormControl, FormLabel, FormGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Log from '../src/Log.png';
 export default function App() {
@@ -10,22 +10,36 @@ export default function App() {
       <Container>
         <Navbar>
           <Container>
-          <img width="100" height="50" src={Log} alt="Log"/>
+            <img width="100" height="50" src={Log} alt="Log" />
             <Navbar.Brand>CertiBlock</Navbar.Brand>
             <Navbar.Toggle></Navbar.Toggle>
             <Navbar.Collapse>
               <Nav>
-                <Nav.Link>login</Nav.Link>
-                <NavDropdown>
-                <NavDropdown.Item>Admin</NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link >login</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <h1>aki ba el formulario de inicio de sesion</h1>
-        <Button>iniciar sesion</Button>
-        <Button>registrarse</Button>
+        <h1>Inicio de Sesion</h1>
+      </Container>
+
+      <Container style={{width:'60%'}}>
+        <Card>
+          <Card.Body>
+            <Form>
+              <FormGroup>
+                <FormLabel>Ingrese su correo electronico</FormLabel>
+                <FormControl placeholder='Ingresar correo'></FormControl>
+                <FormLabel>Ingrese su contraseña</FormLabel>
+                <FormControl placeholder='Ingresar contraseña'></FormControl>
+              </FormGroup>
+            </Form>
+            <center>
+              <Button className='d-flex justify-content-between, mt-3'>Iniciar sesion</Button>
+              <Button className='mt-1'>Registrarse</Button>
+            </center>
+          </Card.Body>
+        </Card>
       </Container>
     </main>
   );
