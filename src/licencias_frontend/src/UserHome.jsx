60ece4ea50714f0058e8a59601423e91ae5defcb
manue1666 <utm23090708@utma.edu.mx>
2024-10-12 23:27:58 -0600

    opciones de usuario

--- src/licencias_frontend/src/UserHome.jsx
+++ src/licencias_frontend/src/UserHome.jsx
@@ -1,34 +1,55 @@
 
-import { Container, Nav, Navbar, NavDropdown,Button, Card } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown,Button, Card,Tab,Tabs } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Log from '../src/Log.png';
+import { FormTram } from './UserOptions';
 
 
 export default function UserHome() {
 
   return (
-    <Container className='mt-3' >
+    <><Container className='mt-3'>
       <Card>
         <Card.Body>
-        <Navbar>
-        <Container>
-          <img width="100" height="50" src={Log} alt="Log" /> 
-          <Navbar.Brand>CertiBlock</Navbar.Brand>
-          <Navbar.Toggle></Navbar.Toggle>
-          <Navbar.Collapse>
-            <Nav>
-              <Nav.Link></Nav.Link>
-              <NavDropdown title="Menu">
-              <NavDropdown.Item>Consultar</NavDropdown.Item>
-              <NavDropdown.Item>Solicitar</NavDropdown.Item>
-              <NavDropdown.Item href='/'>Log out</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+          <Navbar>
+            <Container>
+              <img width="100" height="50" src={Log} alt="Log" />
+              <Navbar.Brand>CertiBlock</Navbar.Brand>
+              <Navbar.Toggle></Navbar.Toggle>
+              <Navbar.Collapse>
+                <Nav>
+                  <Nav.Link></Nav.Link>
+                  <NavDropdown title="Menu">
+                    <NavDropdown.Item>Consultar</NavDropdown.Item>
+                    <NavDropdown.Item>Solicitar</NavDropdown.Item>
+                    <NavDropdown.Item href='/'>Log out</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </Card.Body>
       </Card>
-    </Container>
+    </Container><Container className='mt-3'>
+        <Card>
+          <Card.Body>
+            <Tabs
+              defaultActiveKey="consultar"
+              id="fill-tab-example"
+              className="mb-3"
+              fill
+            >
+              <Tab eventKey="consultar" title="consultar">
+                Aqui aparecera la licencia digital del usuario asi como las multas vinculadas a esta o si requiere renovacion
+              </Tab>
+              <Tab eventKey="tramitar" title="tramitar">
+                <Container>
+                  <FormTram/>
+                </Container>
+              </Tab>
+            </Tabs>
+          </Card.Body>
+        </Card>
+      </Container></>
   );
 }
---
+++ src/licencias_frontend/src/UserOptions.jsx
@@ -0,0 +1,17 @@
+import { Button, Card, Container } from "react-bootstrap"
+
+
+
+export const FormTram=({})=>{
+    return(
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title>Solicita tu licencia digital</Card.Title>
+                    <Card.Text>llena el formulario y envialo y pronto recibiras tu licencia digital en blockchain yujuu</Card.Text>
+                    <Button>Solicitar</Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
