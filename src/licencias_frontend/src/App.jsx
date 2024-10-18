1350e99a7e21677d6b52d3bed570484dc54be54b
Ramirez7u7 <utm23090438@utma.edu.mx>
2024-10-18 17:51:33 -0600

    final

--- src/licencias_frontend/src/App.jsx
+++ src/licencias_frontend/src/App.jsx
@@ -1,34 +1,38 @@
-import { Container, Nav, Navbar, Button, Card, Form, FormControl, FormLabel, FormGroup, Modal } from 'react-bootstrap';
+import React from 'react';
+import { Container, Nav, Navbar, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Log from '/home/ramirez/LICENCIAS/src/licencias_frontend/src/Log.png';
+import Log from '/home/ramirez/LICENCIAS/src/licencias_frontend/src/Log.png';  // Asegúrate de que la ruta sea correcta
+import c5 from '/home/ramirez/LICENCIAS/src/licencias_frontend/src/c5.jpg';
 import { LoginForm } from './Login';
 import { RegistForm } from './Regist';
 import { LoginII } from './LoginII';
 
-
-
+// Lista de usuarios
 export const users = [
   { id: 1, name: 'Admin', surname: 'User', licenseNumber: '0000', email: 'admin@example.com', password: 'admin123', role: 'admin' },
   { id: 2, name: 'Regular', surname: 'User', licenseNumber: '1111', email: 'user@example.com', password: 'user123', role: 'user' }
 ];
 
-window.users = users;  // Exponer el arreglo users en el objeto window
+window.users = users;  // Exponer el arreglo users en el objeto window para acceso global
 
+// Componente principal de la aplicación
 export default function App() {
-
-
   return (
     <main>
+      {/* Barra de navegación */}
       <Container className='mt-3'>
+      
         <Card>
           <Card.Body>
             <Navbar>
               <Container>
+                {/* Logotipo */}
                 <img width="100" height="50" src={Log} alt="Log" />
                 <Navbar.Brand>CertiBlock</Navbar.Brand>
-                <Navbar.Toggle></Navbar.Toggle>
+                <Navbar.Toggle />
                 <Navbar.Collapse>
                   <Nav>
+                    {/* Enlaces adicionales si es necesario */}
                     <Nav.Link></Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
@@ -36,17 +40,24 @@ export default function App() {
             </Navbar>
           </Card.Body>
         </Card>
+        
       </Container>
+
+      {/* Formulario de inicio de sesión */}
       <Container>
-        <LoginForm/>
+        <LoginForm />
       </Container>
+
+      {/* Formulario de registro */}
       <Container>
-        <RegistForm/>
+        <RegistForm />
       </Container>
+      <img width="30%" height="50%" src={c5} alt="c5"  style={{position:"absolute", left:"45%", marginTop:"-23%"}}/>
+
+      {/* Iniciar sesión con Internet Identity */}
       <Container>
-        <LoginII/>
+        {/* <LoginII /> */}
       </Container>
-      
     </main>
   );
 }
